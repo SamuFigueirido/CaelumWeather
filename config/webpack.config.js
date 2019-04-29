65b795e9f0f1668ef64bd02bf31ac57465c189f1
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-04-29 17:18:29 +0200

    feat(Task-1): webpack.config.js created

---
+++ config/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app/app.module.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: { 
+        contentBase: path.join(__dirname, "src"), 
+        compress: true, 
+        port: 9000, 
+        publicPath: "/dist/"
+    }
+}
