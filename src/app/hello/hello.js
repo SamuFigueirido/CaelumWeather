a593f0dff4fdedd529014b3713271a695789e937
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 17:33:45 +0200

    feat(Task-3): edited files

---
+++ src/app/hello/hello.js
@@ -0,0 +1,6 @@
+import template from './hello.html';
+import './hello.css';
+const AppComponent = {
+    template
+};
+export default AppComponent;
---
+++ src/app/world/world.js
@@ -0,0 +1,6 @@
+import template from './world.html';
+import './world.css';
+const AppComponent = {
+    template
+};
+export default AppComponent;
