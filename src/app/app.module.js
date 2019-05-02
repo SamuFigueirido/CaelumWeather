a2a9559ed86ae00370e9a8354f7d6703aad17868
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 17:34:23 +0200

    feat(Task-3): provider added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,6 +1,26 @@
 import angular from 'angular';
 import appComponent from './app.component';
 import uirouter from '@uirouter/angularjs';
-angular.module('app', [
-    uirouter
-]).component('app', appComponent);
+import helloComp from './hello/hello';
+import worldComp from './world/world';
+const myApp = angular.module('app', [uirouter]).component('app', appComponent);
+
+myApp.component('hello', helloComp);
+myApp.component('world', worldComp);
+
+myApp.config(['$stateProvider', function(provider) {
+    const hello = {
+        name: 'hello',
+        url: '/hello',
+        component: 'hello'
+    }
+
+    const world = {
+        name: 'world',
+        url: '/world',
+        component: 'world'
+    }
+    
+    provider.state(hello);
+    provider.state(world);
+}]);
