14136afd0332c5ae9bbd76fe51dff6772c23ae6d
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 10:02:18 +0200

    feat(Task-2): angular-animate added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import appComponent from './app.component';
 import ngMaterial from 'angular-material';
+import ngAnimate from 'angular-animate';
 angular.module('app', [
-  ComponentsModule.name, ngMaterial
+  ComponentsModule.name, ngMaterial, ngAnimate
 ]).component('app', appComponent);
