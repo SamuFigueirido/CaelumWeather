077aa9f01431cbf904ea86834081f677d23737be
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 17:49:18 +0200

    fix(Task-1): pluging configured

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,16 +6,20 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../dist')
     },
-    plugins: [new HtmlWebpackPlugin()],
-    devServer: { 
-        contentBase: path.join(__dirname, '../src'), 
-        compress: true, 
-        port: 9000, 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html'),
+            filename: path.resolve(__dirname, '../dist/index.html')
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, '../src'),
+        compress: true,
+        port: 9000,
         publicPath: '/dist/'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.html$/,
                 use: {
                     loader: 'html-loader',
