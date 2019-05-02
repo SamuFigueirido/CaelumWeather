a4119c936efb4fd6ca076f1aab7763f7755a0f14
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 18:04:22 +0200

    fis(Task-2): empy line added between imports and the rest of the code

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,6 +3,7 @@ import appComponent from './app.component';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
+
 angular.module('app', [
   ngMaterial, ngAnimate, ngAria
 ]).component('app', appComponent);
