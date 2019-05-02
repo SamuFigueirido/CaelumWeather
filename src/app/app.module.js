60811439cd8813d79ed481759b817b3bde89df0c
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 15:18:43 +0200

    feat(Task-3): UI-Router import added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,4 +1,6 @@
 import angular from 'angular';
 import appComponent from './app.component';
+import uirouter from '@uirouter/angularjs';
 angular.module('app', [
+    uirouter
 ]).component('app', appComponent);
