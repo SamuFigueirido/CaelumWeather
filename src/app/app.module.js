62d594e9bdc74953c5fcedd70234d1521580d5fd
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 10:05:48 +0200

    feat(Task-2): angular-aria added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import appComponent from './app.component';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
 angular.module('app', [
-  ComponentsModule.name, ngMaterial, ngAnimate
+  ComponentsModule.name, ngMaterial, ngAnimate, ngAria
 ]).component('app', appComponent);
