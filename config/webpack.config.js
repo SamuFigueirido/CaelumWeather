9d237967407b1ed248fa52aa415ceafe157a6b4b
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-02 09:47:19 +0200

    feat(Task-2): webpack.config.js updated

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -23,10 +23,11 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 use: [
                     "style-loader",
-                    "css-loader"
+                    "css-loader",
+                    "sass-loader"
                 ]
             },
             {
