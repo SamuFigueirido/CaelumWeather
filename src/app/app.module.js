c717bb219ddc99d7c5d4269dc3b6d0af9560b5a8
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-03 13:21:09 +0200

    feat(Task-4): script code added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,3 +1,8 @@
 import angular from 'angular';
 import appComponent from './app.component';
-angular.module('app', []).component('app', appComponent);
+// angular.module('app', []).component('app', appComponent);
+
+    const myApp = angular.module('app', []);
+    myApp.controller('weatherCtrl', function($scope) {
+        const scope = $scope;
+    });
