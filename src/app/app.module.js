b51f330cafa481740a11c60b2ee0f00bba286aa5
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-03 09:12:27 +0200

    feat(Task-3): angular-material, animate and aria added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,7 +3,12 @@ import appComponent from './app.component';
 import uirouter from '@uirouter/angularjs';
 import helloComp from './hello/hello';
 import worldComp from './world/world';
-const myApp = angular.module('app', [uirouter]).component('app', appComponent);
+import ngMaterial from 'angular-material';
+import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
+
+angular.module('app', [ngMaterial, ngAnimate, ngAria]).component('app', appComponent);
+const myApp = angular.module('app', [uirouter, ngMaterial, ngAnimate, ngAria])
 
 myApp.component('hello', helloComp);
 myApp.component('world', worldComp);
