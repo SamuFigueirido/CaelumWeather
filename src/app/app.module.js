a1cfd978644a99624ff2832f75607d889a779852
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-03 14:00:18 +0200

    fix(Task-3): config in another file

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -6,6 +6,7 @@ import worldComp from './world/world';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
+import config from './helloworld';
 
 angular.module('app', [ngMaterial, ngAnimate, ngAria]).component('app', appComponent);
 const myApp = angular.module('app', [uirouter, ngMaterial, ngAnimate, ngAria])
@@ -13,19 +14,4 @@ const myApp = angular.module('app', [uirouter, ngMaterial, ngAnimate, ngAria])
 myApp.component('hello', helloComp);
 myApp.component('world', worldComp);
 
-myApp.config(['$stateProvider', function(provider) {
-    const hello = {
-        name: 'hello',
-        url: '/hello',
-        component: 'hello'
-    }
-
-    const world = {
-        name: 'world',
-        url: '/world',
-        component: 'world'
-    }
-    
-    provider.state(hello);
-    provider.state(world);
-}]);
+myApp.config(config);
