d64d3f9d373ff5066d5a2e608fdb3e39dbb5ddf3
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-06 17:17:06 +0200

    feat(Task-5): deleted files

--- src/app/app.component.js
+++
@@ -1,9 +0,0 @@
-import template from './app.component.html';
-import controller from './app.weatherController';
-import './app.component.scss';
-
-const AppComponent = {
-  template,
-  controller
-};
-export default AppComponent;
--- src/app/hello/hello.js
+++
@@ -1,9 +0,0 @@
-import './hello.css';
-
-export default {
-    controller,
-    bindings: {
-        favorite: '<'
-    },
-    template: require('./hello.html')
-};
