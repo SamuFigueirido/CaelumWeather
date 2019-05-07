e121f244ce00be31ef23a858efb4b430f6d025bf
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-07 16:08:39 +0200

    feat(Task-7): loader added to see icons

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
                 ]
             },
             {
-
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'        
             }
         ]
     }
