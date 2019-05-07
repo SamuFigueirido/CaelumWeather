6ee9c92c74e87d7b65281dd81c771ee9cd89b8ad
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-07 16:10:48 +0200

    feat(Task-7): component added to app.module.js

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,7 +4,11 @@ import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
 import weatherService from './app.weatherService';
+import startPage from './start-page/start-page';
+import "material-icons/iconfont/material-icons.scss";
+import "angular-material/angular-material.scss";
 
 export default angular
-    .module('app', [uirouter, ngMaterial, ngAnimate, ngAria])
+    .module('startpage', [uirouter, ngMaterial, ngAnimate, ngAria])
+    .component('startpage', startPage)
     .factory('weatherService', weatherService);
