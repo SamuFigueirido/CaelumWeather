ce1196a56143b85b9831071602aacf470f429a5e
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-09 14:38:51 +0200

    feat(Task-7): service added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,12 +3,14 @@ import uirouter from '@uirouter/angularjs';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
-import weatherService from './app.weatherService';
+import lastSearchesService from './app.lastSearchesService';
 import startPage from './start-page/start-page';
+import lastSearches from './app.lastSearches';
 import "material-icons/iconfont/material-icons.scss";
 import "angular-material/angular-material.scss";
 
 export default angular
     .module('startpage', [uirouter, ngMaterial, ngAnimate, ngAria])
     .component('startpage', startPage)
-    .factory('weatherService', weatherService);
+    .factory('lastSearchesService', lastSearchesService)
+    .value('lastSearches', lastSearches);
