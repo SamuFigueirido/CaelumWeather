fe8a1d758774fe5cc366900d1cb36844b4be9b3e
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-09 14:40:26 +0200

    feat(Task-7): better way to do the service

---
+++ src/app/app.lastSearchesService.js
@@ -0,0 +1,29 @@
+export default class LastSearchesService {
+    constructor(lastSearches) {
+        this.lastSearches = lastSearches;
+    }
+
+    saveCity(city) {
+        if (city === undefined || city.trim() === '') {
+            return;
+        } else {
+            const cityAux = (city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()).trim();
+            if (this.lastSearches.includes(cityAux)) {
+                return;
+            } else {
+                this.lastSearches.unshift(cityAux);
+                if (this.lastSearches.length > 10) {
+                    this.lastSearches.splice(10, this.lastSearches.length - 10);
+                }
+                localStorage.setItem('lastSearches', JSON.stringify(this.lastSearches));
+            }
+        }
+    }
+
+    getCities() {
+        this.lastSearches = JSON.parse(localStorage.getItem('lastSearches')) || [];
+        return this.lastSearches;
+    }
+}
+
+LastSearchesService.$inject = ['lastSearches'];
