7c08375684d4c5005a14e55e2b4920b768e46417
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-15 16:01:59 +0200

    fix(Task-8): old controllers deleted

--- src/app/main-page/main-page.js
+++ src/app/main-page/main-page.js
@@ -1,10 +1,8 @@
 import template from './main-page.html';
 import './main-page.scss';
-import controller from './main-page.controller';
 
 const AppComponent = {
-    template,
-    controller
+    template
 };
 
 export default AppComponent;
--- src/app/start-page/start-page.controller.js
+++ src/app/start-page/start-page.controller.js
@@ -1,18 +1,1 @@
-export default class StarPageController {
-    constructor(lastSearchesService, $state) {
-        this.lastSearchesService = lastSearchesService;
-        this.$state = $state;
-    }
-
-    searchCity(city) {
-        if(this.lastSearchesService.saveCity(city)) {
-            this.$state.go('mainPage');
-        }
-    }
-
-    showCities() {
-        return this.lastSearchesService.getCities();
-    }
-}
-
-StarPageController.$inject = ['lastSearchesService', '$state'];
+export default {};
