d61a6c26c0a49232404bab228768a57a6e9a5644
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-15 10:38:20 +0200

    feat(Task-8): main-page.js added

---
+++ src/app/main-page/main-page.js
@@ -0,0 +1,11 @@
+import template from './main-page.html';
+import './main-page.scss';
+// import controller from './main-page.controller';
+import controller from '../start-page/start-page.controller';
+
+const AppComponent = {
+    template,
+    controller
+};
+
+export default AppComponent;
