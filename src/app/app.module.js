0a74a6481e10e56c1e77f5b91a167e47e39a3ae0
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-15 10:47:20 +0200

    feat(Task-8): modules added

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -5,15 +5,17 @@ import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
 import lastSearchesService from './services/lastSearches';
 import openWeatherMapsService from './services/openWeatherMaps';
-import startPage from './start-page/start-page';
+import startPageModule from './start-page/start-page.module';
+import mainPageModule from './main-page/main-page.module';
+import config from './app.division';
 import lastSearches from './values/lastSearches';
 import currentWeather from './values/currentWeather';
 import "material-icons/iconfont/material-icons.scss";
 import "angular-material/angular-material.scss";
 
 export default angular
-    .module('startpage', [uirouter, ngMaterial, ngAnimate, ngAria])
-    .component('startpage', startPage)
+    .module('app', [uirouter, ngMaterial, ngAnimate, ngAria, startPageModule, mainPageModule])
+    .config(config)
     .factory('lastSearchesService', lastSearchesService)
     .factory('openWeatherMapsService', openWeatherMapsService)
     .value('lastSearches', lastSearches)
