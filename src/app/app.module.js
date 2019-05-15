fdf443461cd39d69d961a90dbaafb2e038e511e4
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-15 16:37:22 +0200

    fix(Task-8): list of imports sorted

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,18 +1,30 @@
+//Angular
 import angular from 'angular';
-import uirouter from '@uirouter/angularjs';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
+
+//UI-Router and routes
+import uirouter from '@uirouter/angularjs';
+import config from './app.division';
+
+//Services
 import lastSearchesService from './services/lastSearches';
 import openWeatherMapsService from './services/openWeatherMaps';
+
+//Modules
 import startPageModule from './start-page/start-page.module';
 import mainPageModule from './main-page/main-page.module';
-import config from './app.division';
+
+//Values
 import lastSearches from './values/lastSearches';
 import currentWeather from './values/currentWeather';
+
+//External libraries
 import "material-icons/iconfont/material-icons.scss";
 import "angular-material/angular-material.scss";
 
+//Components
 import logoLetters from './components/logo-letters/logo-letters';
 import inputButton from './components/search-input-button/search-input-button';
 import listCities from './components/list-cities/list-cities';
