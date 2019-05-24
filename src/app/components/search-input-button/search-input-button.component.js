23daf553d3c7cfdaca1f5723312f1191444791fb
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-24 16:41:02 +0200

    feat(Task-9): own controller used

--- src/app/components/search-input-button/search-input-button.component.js
+++ src/app/components/search-input-button/search-input-button.component.js
@@ -1,9 +1,9 @@
 import template from './search-input-button.component.html';
 import './search-input-button.component.scss';
-// import controller from './search-input-button.controller';
+import controller from './search-input-button.controller';
 
 const AppComponent = {
-    //controller,
+    controller,
     template
 };
 
