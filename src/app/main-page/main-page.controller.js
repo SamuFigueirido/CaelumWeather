1cc8827ef09dd4ba237a8db814502e05f1857308
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-30 17:53:50 +0200

    feat(Task-10): component added to the main page

--- src/app/main-page/main-page.controller.js
+++ src/app/main-page/main-page.controller.js
@@ -1,9 +1,11 @@
 export default class MainPageController {
-    constructor($state, lastSearchesService, nearbyCitiesService) {
+    constructor($state, lastSearchesService, nearbyCitiesService, openWeatherMapsService) {
         this.$state = $state;
         this.lastSearchesService = lastSearchesService;
         this.nearbyCitiesService = nearbyCitiesService;
+        this.openWeatherMapsService = openWeatherMapsService;
         this.nearbyCities = [];
+        this.weatherDays = ['DAY 1', 'DAY 2', 'DAY 3', 'DAY 4', 'DAY 5'];
     }
 
     $onInit() {
@@ -27,7 +29,17 @@ export default class MainPageController {
             });
         this.nearbyCities = cities;
         console.log('Cities: ', cities);
+
+        this.openWeatherMapsService.getCurrentWeather(this.param)
+        .then(response => {
+            response.forEach(element => {
+                // this.weatherDays.push(element.dt);
+            });
+        })
+        .catch(response => {
+            console.log('There is no weather for this city');
+        });
     }
 }
 
-MainPageController.$inject = ['$state', 'lastSearchesService', 'nearbyCitiesService'];
+MainPageController.$inject = ['$state', 'lastSearchesService', 'nearbyCitiesService', 'openWeatherMapsService'];
