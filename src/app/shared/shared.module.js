e3a4864090c3bff9ea1b505e79c731c33a3d5e1b
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-30 12:58:14 +0200

    fix(Task-10): route changed

--- src/app/shared/shared.module.js
+++ src/app/shared/shared.module.js
@@ -13,7 +13,7 @@ import currentWeather from '../values/currentWeather';
 import logoLetters from '../components/logo-letters/logo-letters.component';
 import inputButton from '../components/search-input-button/search-input-button.component';
 import listCities from '../components/list-cities/list-cities.component';
-import weatherDay from '../components/weater-day/weather-day.component';
+import weatherDay from '../components/weather-day/weather-day.component';
 
 export default angular
     .module('appShared', [])
