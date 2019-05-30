3340f8448a99e1003a22c23af13737ade48dfb7e
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-05-30 13:42:15 +0200

    fix(Task-10): new name refactored

---
+++ src/app/components/container-days/container-days.component.js
@@ -0,0 +1,10 @@
+import template from './container-days.component.html';
+import './container-days.component.scss';
+import controller from './container-days.controller';
+
+const AppComponent = {
+    controller,
+    template
+};
+
+export default AppComponent;
