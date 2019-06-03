e80b32bdd0b4c0499cafbe4379a7150b6be5f779
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-06-03 12:58:32 +0200

    feat(Task-10): data weather from cities saved

--- src/app/main-page/main-page.controller.js
+++ src/app/main-page/main-page.controller.js
@@ -5,7 +5,10 @@ export default class MainPageController {
         this.nearbyCitiesService = nearbyCitiesService;
         this.openWeatherMapsService = openWeatherMapsService;
         this.nearbyCities = [];
-        this.weatherDays = ['DAY 1', 'DAY 2', 'DAY 3', 'DAY 4', 'DAY 5'];
+
+        this.nameDays = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+        this.weatherDays = [];
+        this.days = [];
     }
 
     $onInit() {
@@ -31,11 +34,67 @@ export default class MainPageController {
         console.log('Cities: ', cities);
 
         this.openWeatherMapsService.getCurrentWeather(this.param)
-        .then(response => {
-            response.forEach(element => {
-                // this.weatherDays.push(element.dt);
-            });
-        })
+            .then(response => {
+                console.log('Length:', response.length);
+                const days = [];
+                let cont = -1;
+                response.forEach(element => {
+                    let flag = false;
+                    const day = new Date(element.dt * 1000).getDate();
+                    days.forEach(item => {
+                        if (item.day === day) {
+                            flag = true;
+                        }
+                    });
+                    if (!flag) {
+                        cont++;
+                        days.push({
+                            main: element.dt * 1000,
+                            day: day,
+                            hours: []
+                        })
+                    };
+                    const hourObject = {
+                        hour: (element.dt * 1000),
+                        temperature: {
+                            main: element.main.temp,
+                            temp_max: element.main.temp_max,
+                            temp_min: element.main.temp_min
+                        },
+                        humidity: element.main.humidity,
+                        weather: {
+                            main: element.weather.main,
+                            description: element.weather.description,
+                            icon: element.weather.icon
+                        },
+                        clouds: element.clouds.all,
+                        wind: {
+                            speed: element.wind.speed,
+                            direction: element.wind.deg
+                        }
+                    };
+                    days[cont].hours.push(hourObject);
+                });
+                days.forEach(day => {
+                    // const date = new Date(day.main).getDate() + '/' + new Date(day.main).getMonth() + '/' + new Date(day.main).getFullYear();
+                    const index = new Date(day.main).getDay() - 1;
+                    let value = '';
+                    switch (index) {
+                        case 0:
+                                value = 'TODAY';
+                            break;
+                        case 1:
+                                value = 'TOMORROW';
+                            break;
+                        default:
+                                value = this.nameDays[index];
+                            break;
+                    }
+                    this.days.push(value);
+                });
+
+                console.log('Days:', days);
+            })
         .catch(response => {
             console.log('There is no weather for this city');
         });
