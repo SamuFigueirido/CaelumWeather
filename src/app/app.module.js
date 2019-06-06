7fb98eacb7a18eeec3deb114a33cdcd76c8a4795
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-06-06 17:59:27 +0200

    feat(Task-15): ngRedux added to app.module

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,6 +2,7 @@ import "material-icons/iconfont/material-icons.scss";
 import "angular-material/angular-material.scss";
 import angular from 'angular';
 import uirouter from '@uirouter/angularjs';
+import ngRedux from 'ng-redux';
 import ngMaterial from 'angular-material';
 import ngAnimate from 'angular-animate';
 import ngAria from 'angular-aria';
@@ -16,6 +17,6 @@ import mainPageModule from './main-page/main-page.module';
 import sharedModule from './shared/shared.module';
 
 export default angular
-    .module('app', [uirouter, ngMaterial, ngAnimate, ngAria, ngMessages, startPageModule, mainPageModule, sharedModule])
+    .module('app', [uirouter, ngRedux, ngMaterial, ngAnimate, ngAria, ngMessages, startPageModule, mainPageModule, sharedModule])
     .config(config)
     .name;
