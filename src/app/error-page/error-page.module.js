b90d95e7785299bee46a4ea3a37d0e974b39a13d
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-06-14 09:40:44 +0200

    feat(Task-11): errorPage deleted

--- src/app/error-page/error-page.module.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import errorPage from './error-page.component';
-
-export default angular
-    .module('appErrorPage', [])
-    .component('errorPage', errorPage)
-    .name;
