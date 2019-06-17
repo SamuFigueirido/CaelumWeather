167ec7cf6ddc61713acb9efd7d67724af60265c0
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-06-17 14:39:07 +0200

    feat(Task-20): translations of the contact-dialog

---
+++ src/resources/translations/en_US.js
@@ -0,0 +1,15 @@
+export default {
+    "CONTACT_DIALOG": {
+        "TITLE": "Contact Support",
+        "INPUT_NAME": "Name",
+        "INPUT_SUBJECT": "Subject",
+        "INPUT_ISSUE": "Issue",
+        "TOOLTIP": {
+            "NAME": "Your name here",
+            "SUBJECT": "Subject of the issue",
+            "ISSUE": "The issue summarized in three lines"
+        },
+        "BUTTON_SUBMIT": "Submit",
+        "BUTTON_CANCEL": "Cancel"
+    },
+}
---
+++ src/resources/translations/es_ES.js
@@ -0,0 +1,15 @@
+export default {
+    "CONTACT_DIALOG": {
+        "TITLE": "Soporte técnico",
+        "INPUT_NAME": "Nombre",
+        "INPUT_SUBJECT": "Motivo",
+        "INPUT_ISSUE": "Problema",
+        "TOOLTIP": {
+            "NAME": "Indica tu nombre",
+            "SUBJECT": "Motivo de la queja",
+            "ISSUE": "El problema resumido en tres líneas"
+        },
+        "BUTTON_SUBMIT": "Aceptar",
+        "BUTTON_CANCEL": "Cancelar"
+    },
+}
