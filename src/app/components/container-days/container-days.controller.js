176e0254ba58499637ef34201236b7ff439f33e6
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-06-17 09:12:37 +0200

    fix(Task-11): deleted return on $onInit()

--- src/app/components/container-days/container-days.controller.js
+++ src/app/components/container-days/container-days.controller.js
@@ -11,13 +11,12 @@ export default class ContainerDaysController {
             this.param = this.$state.params.city.toLowerCase();
         }
         this.openWeatherMapsService.getCurrentWeather(this.param)
-        .then(response => {
-            this.listDays = this.weatherContainerService.getListToShow(response);
-        })
-        .catch(response => {
-            this.$state.go('errorInfo', {city: this.param, status: response.status});
-        });
-        return this.listDays;
+            .then(response => {
+                this.listDays = this.weatherContainerService.getListToShow(response);
+            })
+            .catch(response => {
+                this.$state.go('errorInfo', {city: this.param, status: response.status});
+            });
     }
 }
 
